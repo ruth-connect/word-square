3aa2cf908bf0b9336e2614aa8f6ad803b4594d05
ruth <ruth@DESKTOP-HOVL7PT>
2023-03-10 19:34:03 +0000

    Create predicate class for word length.

---
+++ src/main/java/uk/me/ruthmills/wordsquare/dictionary/WordLengthPredicate.java
@@ -0,0 +1,34 @@
+package uk.me.ruthmills.wordsquare.dictionary;
+
+import java.util.function.Predicate;
+
+/**
+ * Predicate class to check if a string is of the required length.
+ */
+public class WordLengthPredicate implements Predicate<String> {
+
+	/**
+	 * Length of string to match.
+	 */
+	private int length;
+
+	/**
+	 * Constructor. Takes the number of letters in the string we want to match.
+	 * 
+	 * @param length The required length (in number of letters) of the string.
+	 */
+	public WordLengthPredicate(int length) {
+		this.length = length;
+	}
+
+	/**
+	 * Check if the supplied word contains the required number of letters.
+	 * 
+	 * @param word The word to test.
+	 * @return true if the word has the required number of letters, or false if not.
+	 */
+	@Override
+	public boolean test(String word) {
+		return word.length() == length;
+	}
+}
---
+++ src/test/java/uk/me/ruthmills/wordsquare/dictionary/WordLengthPredicateTest.java
@@ -0,0 +1,74 @@
+package uk.me.ruthmills.wordsquare.dictionary;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test suite for the Word Length Predicate class.
+ * 
+ * @author ruth
+ */
+public class WordLengthPredicateTest {
+
+	// Expected word length to test.
+	private static final int EXPECTED_WORD_LENGTH = 5;
+
+	// The Word Length Predicate class under test.
+	private WordLengthPredicate wordLengthPredicate;
+
+	/**
+	 * Set up the test dependencies.
+	 */
+	@Before
+	public void setUp() {
+		wordLengthPredicate = new WordLengthPredicate(EXPECTED_WORD_LENGTH);
+	}
+
+	/**
+	 * Test that the predicate returns false if the word is too short.
+	 */
+	@Test
+	public void shouldReturnFalse_whenWordIsTooShort() {
+		// given
+		String word = "frog";
+
+		// when
+		boolean isWordCorrectLength = wordLengthPredicate.test(word);
+
+		// then
+		assertThat(isWordCorrectLength, is(false));
+	}
+
+	/**
+	 * Test that the predicate returns true if the word is the expected length.
+	 */
+	@Test
+	public void shouldReturnTrue_whenWordIsExpectedLength() {
+		// given
+		String word = "kebab";
+
+		// when
+		boolean isWordCorrectLength = wordLengthPredicate.test(word);
+
+		// then
+		assertThat(isWordCorrectLength, is(true));
+	}
+
+	/**
+	 * Test that the predicate returns false if the word is too long.
+	 */
+	@Test
+	public void shouldReturnFalse_whenWordIsTooLong() {
+		// given
+		String word = "guitar";
+
+		// when
+		boolean isWordCorrectLength = wordLengthPredicate.test(word);
+
+		// then
+		assertThat(isWordCorrectLength, is(false));
+	}
+}
