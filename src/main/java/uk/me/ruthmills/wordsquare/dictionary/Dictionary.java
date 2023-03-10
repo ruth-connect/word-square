f731f4a14d1bbd1d7b551cebbdda6e112beb6614
ruth <ruth@DESKTOP-HOVL7PT>
2023-03-10 19:09:13 +0000

    Add function to filter words by length.

--- src/main/java/uk/me/ruthmills/wordsquare/dictionary/Dictionary.java
+++ src/main/java/uk/me/ruthmills/wordsquare/dictionary/Dictionary.java
@@ -21,12 +21,29 @@ public class Dictionary {
 	 * Read all the words from the dictionary file.
 	 * 
 	 * @return A list of all the words from the dictionary file.
-	 * @throws IOException Thrown if we cannot read from the dictionary file.
+	 * @throws IOException        Thrown if we cannot read from the dictionary file.
+	 * @throws URISyntaxException Thrown if there is a problem with the URI syntax.
 	 */
 	public List<String> getAllWords() throws IOException, URISyntaxException {
 		try (Stream<String> linesStream = Files
 				.lines(Paths.get(ClassLoader.getSystemResource(DICTIONARY_TEXT_FILE_NAME).toURI()))) {
 			return linesStream.map(word -> word.toString()).collect(Collectors.toList());
+		}
+	}
+
+	/**
+	 * Read only the words matching the required length from the dictionary file.
+	 * 
+	 * @param length The required length of the words to return.
+	 * @return A list of only those words matching the required length.
+	 * @throws IOException        Thrown if we cannot read from the dictionary file.
+	 * @throws URISyntaxException Thrown if there is a problem with the URI syntax.
+	 */
+	public List<String> getWordsOfMatchingLength(int length) throws IOException, URISyntaxException {
+		try (Stream<String> linesStream = Files
+				.lines(Paths.get(ClassLoader.getSystemResource(DICTIONARY_TEXT_FILE_NAME).toURI()))) {
+			return linesStream.map(word -> word.toString()).filter(word -> word.length() == length)
+					.collect(Collectors.toList());
 		}
 	}
 }
--- src/test/java/uk/me/ruthmills/wordsquare/dictionary/DictionaryTest.java
+++ src/test/java/uk/me/ruthmills/wordsquare/dictionary/DictionaryTest.java
@@ -1,7 +1,10 @@
 package uk.me.ruthmills.wordsquare.dictionary;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.text.CharSequenceLength.hasLength;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -16,6 +19,9 @@ import org.junit.Test;
  * @author ruth
  */
 public class DictionaryTest {
+
+	private static final int EXPECTED_NUMBER_OF_ALL_WORDS = 172820;
+	private static final int EXPECTED_NUMBER_OF_4_LETTER_WORDS = 3903;
 
 	// The Dictionary object under test.
 	private Dictionary dictionary;
@@ -39,6 +45,24 @@ public class DictionaryTest {
 		List<String> words = dictionary.getAllWords();
 
 		// then
-		assertThat(words, hasSize(172820)); // expected number of all the words!
+		assertThat(words, hasSize(EXPECTED_NUMBER_OF_ALL_WORDS)); // expected number of all the words!
+	}
+
+	/**
+	 * Test that we can read only the words that are 4 letters long. We filter out
+	 * all other words that are not of the required length.
+	 */
+	@Test
+	public void shouldReadOnly4LetterWordsFromDictionaryTextFile() throws IOException, URISyntaxException {
+		// given
+		int requiredWordLength = 4;
+
+		// when
+		List<String> words = dictionary.getWordsOfMatchingLength(requiredWordLength);
+
+		// then
+		assertThat(words.size(), lessThan(EXPECTED_NUMBER_OF_ALL_WORDS)); // will be less than all the words!
+		assertThat(words, everyItem(hasLength(4))); // every word is 4 letters long.
+		assertThat(words, hasSize(EXPECTED_NUMBER_OF_4_LETTER_WORDS)); // check the total number of 4 letter words.
 	}
 }
