58e2d98d6a2094f19de2cf7236f5f342701065d9
ruth <ruth@DESKTOP-HOVL7PT>
2023-03-11 12:48:41 +0000

    Test how many words we have in the shortlist for each puzzle.

--- src/test/java/uk/me/ruthmills/wordsquare/solution/WordShortlistTest.java
+++ src/test/java/uk/me/ruthmills/wordsquare/solution/WordShortlistTest.java
@@ -38,4 +38,68 @@ public class WordShortlistTest {
 		assertThat(wordShortlist.get(4), is("odd"));
 		assertThat(wordShortlist.get(5), is("oxo"));
 	}
+
+	/**
+	 * Should return the word shortlist for the 4-letter test case.
+	 */
+	@Test
+	public void shouldReturnWordShortlist_for4LetterTestCase() throws IOException, URISyntaxException {
+		// given
+		final int length = 4;
+		final String availableLetters = "aaccdeeeemmnnnoo";
+
+		// when
+		final List<String> wordShortlist = WordShortlist.getWordShortlist(length, availableLetters);
+
+		// then
+		assertThat(wordShortlist, hasSize(74));
+	}
+
+	/**
+	 * Should return the word shortlist for the first 5-letter test case.
+	 */
+	@Test
+	public void shouldReturnWordShortlist_forFirst5LetterTestCase() throws IOException, URISyntaxException {
+		// given
+		final int length = 5;
+		final String availableLetters = "aaaeeeefhhmoonssrrrrttttw";
+
+		// when
+		final List<String> wordShortlist = WordShortlist.getWordShortlist(length, availableLetters);
+
+		// then
+		assertThat(wordShortlist, hasSize(653));
+	}
+
+	/**
+	 * Should return the word shortlist for the second 5-letter test case.
+	 */
+	@Test
+	public void shouldReturnWordShortlist_forSecond5LetterTestCase() throws IOException, URISyntaxException {
+		// given
+		final int length = 5;
+		final String availableLetters = "aabbeeeeeeeehmosrrrruttvv";
+
+		// when
+		final List<String> wordShortlist = WordShortlist.getWordShortlist(length, availableLetters);
+
+		// then
+		assertThat(wordShortlist, hasSize(495));
+	}
+
+	/**
+	 * Should return the word shortlist for the 7-letter test case.
+	 */
+	@Test
+	public void shouldReturnWordShortlist_for7LetterTestCase() throws IOException, URISyntaxException {
+		// given
+		final int length = 7;
+		final String availableLetters = "aaaaaaaaabbeeeeeeedddddggmmlloooonnssssrrrruvvyyy";
+
+		// when
+		final List<String> wordShortlist = WordShortlist.getWordShortlist(length, availableLetters);
+
+		// then
+		assertThat(wordShortlist, hasSize(2151));
+	}
 }
