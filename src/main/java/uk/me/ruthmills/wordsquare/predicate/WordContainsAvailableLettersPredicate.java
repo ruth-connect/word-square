c7eb0730fe45887951f2c8d4c1d0e21e0e9a2474
ruth <ruth@DESKTOP-HOVL7PT>
2023-03-11 16:56:04 +0000

    Massive performance improvement - use HashBag instead of recursion.

--- src/main/java/uk/me/ruthmills/wordsquare/predicate/WordContainsAvailableLettersPredicate.java
+++ src/main/java/uk/me/ruthmills/wordsquare/predicate/WordContainsAvailableLettersPredicate.java
@@ -2,6 +2,9 @@ package uk.me.ruthmills.wordsquare.predicate;
 
 import java.util.function.Predicate;
 
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.bag.HashBag;
+
 /**
  * Predicate class to check if a word contains only the letters available.
  * 
@@ -31,42 +34,21 @@ public class WordContainsAvailableLettersPredicate implements Predicate<String> 
 	 */
 	@Override
 	public boolean test(final String word) {
-		// Call the recursive function to check if the word contains ONLY a subset of
-		// the available letters.
-		return wordContainsAvailableLetters(word, letters);
-	}
-
-	/**
-	 * Private recursive function to check if the word contains ONLY a subset of the
-	 * available letters.
-	 * 
-	 * @param word             The word to check.
-	 * @param availableLetters The available letters.
-	 * @return true if the word contains ONLY a subset of the available letters.
-	 *         false if not.
-	 */
-	private boolean wordContainsAvailableLetters(final String word, final String availableLetters) {
-		final String firstLetter = word.substring(0, 1); // get the first letter of the word.
-
-		// Do the available letters contain the first letter of the word?
-		if (availableLetters.contains(firstLetter)) {
-			// Yes they do.
-
-			// Have we reached the last letter of the word we are checking?
-			if (word.length() == 1) {
-				// Yes we have. All letters in the word are from the available letters. Return
-				// true.
-				return true;
-			} else {
-				// Work out which letters are still available.
-				final String remainingLetters = availableLetters.replaceFirst(firstLetter, "");
-
-				// Recurse until we reach the last letter of the word.
-				return wordContainsAvailableLetters(word.substring(1, word.length()), remainingLetters);
-			}
-		} else {
-			// No they don't contain the first letter of the word. Return false.
-			return false;
+		// Create a hash bag of available letters.
+		Bag<Byte> availableLetters = new HashBag<Byte>();
+		for (Byte letter : letters.getBytes()) {
+			availableLetters.add(letter);
 		}
+
+		// Iterate through each letter in the word.
+		for (Byte letter : word.getBytes()) {
+			// If the letter is not in the available letters, return false.
+			if (!availableLetters.remove(letter, 1)) {
+				return false;
+			}
+		}
+
+		// All the letters in the word are available. Return true.
+		return true;
 	}
 }
